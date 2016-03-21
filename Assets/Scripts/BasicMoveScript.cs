231898b39a6c59ef1ff1754a5c45aa50d4385993
JBorders3010 <bordersjb@siu.edu>
2016-03-21 12:54:21 -0500

    Added files via upload

--- Assets/Scripts/BasicMoveScript.cs
+++ Assets/Scripts/BasicMoveScript.cs
@@ -4,62 +4,61 @@ using System.Collections;
 public class BasicMoveScript : MonoBehaviour {
 
     public Vector3 startPos, endPos;    //determine the bounds of motion
-    public bool yHeight, xHeight, zHeight; //specify the axis to act as height.  Pieces arc within the height axis, but stay linear in the others
-    public float speed; //number of frames it takes for piece to move from start to end
+    //public bool yHeight = true, xHeight = false, zHeight = false; //specify the axis to act as height.  Pieces arc within the height axis, but stay linear in the others
+    //public float speed; //number of frames it takes for piece to move from start to end
     private float deltaX, deltaY, deltaZ, maxX, maxY, maxZ;   //variables for move equation
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start() {
         //failsafe: if more than one height option is selected by tester, the booleans are prioritized: Y > X > Z
-        if (yHeight && xHeight)
+        //failsafe: if no height option is selected, Y is made default
+        /*if (yHeight && xHeight) {
             xHeight = false;
-        if (yHeight && zHeight)
+            Debug.Log("xHeight/yHeight conflict.  Resolved to yHeight.");
+        } if (yHeight && zHeight) {
             zHeight = false;
-        if (xHeight && zHeight)
+            Debug.Log("zHeight/yHeight conflict.  Resolved to yHeight.");
+        } if (xHeight && zHeight) {
             zHeight = false;
+            Debug.Log("xHeight/zHeight conflict.  Resolved to xHeight.");
+        } if (!xHeight && !yHeight && !zHeight) { 
+            yHeight = true;
+            Debug.Log("Height opt not set.  Resolved to yHeight.");
+        }*/
         deltaX = endPos.x - startPos.x;
+        Debug.Log("deltaX solved.  Value = " + deltaX);
         deltaY = endPos.y - startPos.y;
+        Debug.Log("deltaY solved.  Value = " + deltaY);
         deltaZ = endPos.z - startPos.z;
-        if (xHeight)
+        Debug.Log("deltaZ solved.  Value = " + deltaZ);
+        /*if (xHeight)
             maxX = (deltaX / 2) + 0.75f;
-        else
+        else*/
             maxX = endPos.x;
-        if (yHeight)
+        Debug.Log("maxX solved.  Value = " + maxX);
+        //if (yHeight)
             maxY = (deltaY / 2) + 0.75f;
-        else
-            maxY = endPos.y;
-        if (zHeight)
+        /*else
+            maxY = endPos.y;*/
+        Debug.Log("maxY solved.  Value = " + maxY);
+        /*if (zHeight)
             maxZ = (deltaZ / 2) + 0.75f;
-        else
+        else*/
             maxZ = endPos.z;
+        Debug.Log("maxZ solved.  Value = " + maxZ);
     }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 position = transform.position;
-        while (position != endPos)
+        Debug.Log("Current position: (" + position.x + ", " + position.y + ", " + position.z + ")");
+        bool doMove = false;
+        if (doMove)
         {
-            if (xHeight)
-            {
-                if (position.x != maxX)
-                    transform.Translate(new Vector3(((2 * maxX) / speed), (deltaY / speed), (deltaZ / speed)) * Time.deltaTime);
-                else
-                    transform.Translate(new Vector3(((0 - 1) *((2 * maxX) / speed)), (deltaY / speed), (deltaZ / speed)) * Time.deltaTime);
-            }
-            else if (yHeight)
-            {
-                if (position.y != maxY)
-                    transform.Translate(new Vector3((deltaX / speed), ((2 * maxY) / speed), (deltaZ / speed)) * Time.deltaTime);
-                else
-                    transform.Translate(new Vector3((deltaX / speed), ((0 - 1) * ((2 * maxY) / speed)), (deltaZ / speed)) * Time.deltaTime);
-            }
-            else if (zHeight)
-            {
-                if (position.x != maxX)
-                    transform.Translate(new Vector3((deltaX / speed), (deltaY / speed), ((2 * maxZ) / speed)) * Time.deltaTime);
-                else
-                    transform.Translate(new Vector3((deltaX / speed), (deltaY / speed), ((0 - 1) * ((2 * maxZ) / speed))) * Time.deltaTime);
-            }
+            if (position.y < maxY)
+                transform.Translate(new Vector3(deltaX, (4 * maxY), deltaZ) * Time.deltaTime);
+            else
+                transform.Translate(new Vector3(deltaX, ((0 - 1) * (4 * maxY)), deltaZ) * Time.deltaTime);
         }
 	}
 
