42a4fc779a1f7d74e9d868b7b716eab049bed23b
JBorders3010 <bordersjb@siu.edu>
2016-04-29 20:00:18 -0500

    pt8
    Assets 1

--- Assets/Classes/PieceClass.cs
+++ Assets/Classes/PieceClass.cs
@@ -1,8 +1,28 @@
-﻿public class PieceClass {
-    private string name { get; set; }
+﻿using System.Collections.Generic;
 
-    public PieceClass ()
+public class PieceClass {
+    private string Name;
+    private int[] Location;
+
+    public PieceClass(string name, int[] location)
     {
+        Name = name;
+        Location = location;
+    }
 
+    public int[] GetLocation() {
+        return Location;
+    }
+
+    public string GetName() {
+        return Name;
+    }
+
+    public void Move(int[] NewLocation) {
+        Location = NewLocation;
+    }
+
+    public void SetPiece(int[] Location) {
+        this.Location = Location;
     }
 }
--- Assets/Classes/PlayerClass.cs
+++ Assets/Classes/PlayerClass.cs
@@ -1,6 +1,84 @@
 ﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PlayerClass {
     private List<PieceClass> Pieces;
+    private bool movedKing;
+    private List<GameObject> InGamePieces;
 
+    public PlayerClass() {
+        Pieces = new List<PieceClass>();
+        movedKing = false;
+        InGamePieces = new List<GameObject>();
+    }
+
+    private void AddPiece(string Name, int[] Location) {
+        Pieces.Add(new PieceClass(Name, Location));
+    }
+
+    public void AddPiece(GameObject Piece, string Name) {
+        AddPiece(Name, new int[] { (int)(Piece.transform.position.x + 3.5f), (int)(Piece.transform.position.z + 3.5f) } );
+        InGamePieces.Add(Piece);
+    }
+
+    public void RemovePiece(GameObject Piece)
+    {
+        
+    }
+
+    public void Move(int[] PiecePosition, int[] newPosition) {
+        
+    }
+
+    public List<int[]> AvailableMoves(int[] PiecePosition) {
+        List<int[]> AvailableMoves = new List<int[]>();
+        string type = GetPieceType(PiecePosition);
+        if (type.Equals("PAWN"))
+        {
+            AvailableMoves.Add(new int[] { PiecePosition[0] + 1, PiecePosition[1] });
+            if (PiecePosition[1] == 1)
+                AvailableMoves.Add(new int[] { PiecePosition[0] + 2, PiecePosition[1] });
+        }
+        return AvailableMoves;
+    }
+
+    public List<int[]> GetPiecePositions() {
+        List<int[]> Positions = new List<int[]>();
+        foreach (PieceClass Piece in Pieces)
+            Positions.Add(Piece.GetLocation());
+        return Positions;
+    }
+
+    public string GetPieceType(int[] PiecePosition) {
+        int i = 0;
+        while (i < Pieces.Count)
+        {
+            PieceClass piece = Pieces.ToArray()[i];
+            if (piece.GetLocation() == PiecePosition)
+                return piece.GetName();
+        }
+        return null;
+    }
+
+    public bool KingMoved()
+    {
+        return movedKing;
+    }
+
+    public List<GameObject> GetPieces()
+    {
+        return InGamePieces;
+    }
+
+    public GameObject GetKing()
+    {
+        foreach(GameObject piece in InGamePieces)
+        {
+            if (GetPieceType(new int[] { (int)(piece.transform.position.x + 3.5f), (int)(piece.transform.position.z + 3.5f) }).Equals("KING"))
+            {
+                return piece;
+            }
+        }
+        return null;
+    }
 }
